cfaa14bbc0557db4d583d2c183a942bbb2fb1216
kushalsdesk <kushal99mondal@gmail.com>
2024-02-25 22:15:07 +0530

    Server

--- 20modules/main.go
+++ 20modules/main.go
@@ -10,10 +10,14 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Modules in Go")
-
+	greet()
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func greet() {
+	fmt.Println("Hello From the Server")
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
